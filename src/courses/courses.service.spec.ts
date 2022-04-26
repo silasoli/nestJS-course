056b2a1afce5d84927fe4648f1fe19bad2b62c63
Silasoli <silas.oliveira@blockhub.builders>
2022-04-26 11:36:59 -0300

    Adicionando testes unitarios

---
+++ src/courses/courses.service.spec.ts
@@ -0,0 +1,59 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection, Repository } from 'typeorm';
+import { CoursesService } from './courses.service';
+import { Course } from './entities/course.entity';
+import { Tag } from './entities/tag.entity';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+    findOne: jest.fn(),
+});
+
+describe('CoursesService', () => {
+    let service: CoursesService;
+    let courseRepository: MockRepository;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CoursesService,
+                { provide: Connection, useValue: {} },
+                {
+                    provide: getRepositoryToken(Course),
+                    useValue: createMockRepository()
+                },
+                {
+                    provide: getRepositoryToken(Tag),
+                    useValue: createMockRepository()
+                }
+            ],
+        }).compile();
+
+        service = module.get<CoursesService>(CoursesService);
+        courseRepository = module.get<MockRepository>(getRepositoryToken(Course));
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('findOne', () => {
+        describe('Buscar curso pelo ID', () => {
+            it('Deve retornar o objeto Course', async () => {
+                const courseId = '1';
+                const expectedCourse = {};
+
+                courseRepository.findOne.mockReturnValue(expectedCourse)
+                const course = await service.findOne(courseId)
+                expect(course).toEqual(expectedCourse);
+            });
+
+            it('Deve retornar um NotFoundException', () => {
+                expect(service).toBeDefined();
+            });
+        });
+    });
+});
