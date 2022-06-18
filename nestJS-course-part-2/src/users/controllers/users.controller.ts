f4157628ce4e3e2dbf90ca57b15bdf00d7566b90
Silasoli <silas.oliveira@blockhub.builders>
2022-06-18 18:47:30 -0300

    Finalizando controller e service de users

--- nestJS-course-part-2/src/users/controllers/users.controller.ts
+++ nestJS-course-part-2/src/users/controllers/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { SigninDto } from '../dto/signin.dto';
+import { SignupDto } from '../dto/signup.dto';
+import { User } from '../models/users.model';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
+    @Post('signup')
+    @HttpCode(HttpStatus.CREATED)
+    public async signup(@Body() signupDto: SignupDto): Promise<User> {
+        return this.usersService.signup(signupDto)
+    }
+
+    @Post('signin')
+    @HttpCode(HttpStatus.OK)
+    public async signin(@Body() signinDto: SigninDto): Promise<{ name: string, jwtToken: string, email: string }> {
+        return this.usersService.signin(signinDto);
+    }
+
+    @Get()
+    @UseGuards(AuthGuard('jwt'))
+    @HttpCode(HttpStatus.OK)
+    public async findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
+
+}
