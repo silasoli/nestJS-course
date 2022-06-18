0b86223e77eb5798734899497bb9cecfddf54fc8
Silasoli <silas.oliveira@blockhub.builders>
2022-06-18 11:52:32 -0300

    Organizando estrutura do repo

---
+++ nestJS-course-part-1/src/courses/entities/course.entity.ts
@@ -0,0 +1,35 @@
+import { BeforeInsert, Column, CreateDateColumn, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Tag } from "./tag.entity";
+import { v4 as uuidv4 } from "uuid";
+
+
+@Entity({ name: 'courses' })
+export class Course {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @JoinTable({ name: 'courses_tags' })
+    @ManyToMany(() => Tag, (tag: Tag) => tag.courses,{
+        cascade: true,
+    })
+    tags: Tag[];
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at: Date;
+
+    @BeforeInsert()
+    generatedId(){
+        if (this.id) {
+            return;
+        }
+
+        this.id = uuidv4();
+    }
+}
