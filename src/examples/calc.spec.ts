47af3ce7724d0f843e2b2eef00c7a0384d24ad6e
Silasoli <silas.oliveira@blockhub.builders>
2022-06-18 11:53:37 -0300

    excluindo arquivos

--- src/examples/calc.spec.ts
+++
@@ -1,9 +0,0 @@
-export function add(x: number, y: number) {
-    return x + y;
-}
-
-describe('Initial test', () => {
-    test('add function', () => {
-        expect(add(1, 2)).toEqual(3);
-    });
-});
