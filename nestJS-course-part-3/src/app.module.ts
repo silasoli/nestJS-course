60625cb21b218ab03e2ed4452eb148311aa2b401
Silasoli <silas.oliveira@blockhub.builders>
2022-06-24 15:36:13 -0300

    Adicionando prisma ao projeto

--- nestJS-course-part-3/src/app.module.ts
+++ nestJS-course-part-3/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
 
